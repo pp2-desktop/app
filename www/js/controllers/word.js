7156328b6858ba1ccf146512b689cdc7c03f7e95
pp2 <pp2@05day.com>
2015-09-01 10:35:13 +0900

    a

--- www/js/controllers/word.js
+++ www/js/controllers/word.js
@@ -1,10 +1,16 @@
 angular.module('starter.controllers')
   .controller('word_ctrl', ['$scope', '$rootScope', '$http', function($scope, $rootScope, $http) {
-    
+
     $scope.word_container = [];
-    for(var i=0; i<100; i++) {
-      var f = 'aa' + i;
-      $scope.word_container.push(f);
-    }
+    
+    var init = function() {
+      for(var i=0; i<100; i++) {
+	var obj = {};
+	$scope.word_container.push(obj);
+      }
+      
+    };
+     
+    init();
     
   }]);
